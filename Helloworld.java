0cd29249471df0b2ac7a83c6824fbbcbe9b53e36
Sandeveni Saikrishna <sandeveniskyair@gmail.com>
2025-07-07 20:59:55 +0530

    HelloWorld Program

--- Helloworld.java
+++ Helloworld.java
@@ -2,9 +2,24 @@ package GithubLearn;
 
 public class Helloworld {
 	public static void main(String[] args) {
-		System.out.println("Hello World");
-		System.out.println("Sai");
-		System.out.println("Prashanth");
-		System.out.println("SKY");
+		String s = "a3b2c1";
+//		String res="";
+		StringBuilder res = new StringBuilder();
+		char letter = '0';
+		for (int i = 0; i < s.length(); i++) {
+			char c = s.charAt(i);
+
+			if (Character.isLetter(c)) {
+				letter = c;
+			} else {
+				int num = c - '0';
+
+				for (int j = 0; j < num; j++) {
+					res.append(letter);
+				}
+			}
+		}
+		System.out.println(res);
+
 	}
 }
